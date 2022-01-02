5e3fee7936fcf0b605603f1c026a65847ea0cae1
Dinusha  Withanage <dinushaw@Dinushas-iMac.local>
2022-01-02 14:58:56 +1100

    Layout modifications* Added Column layout * Cat, Child and Dog friendly icons added git commit --m

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+//   outputDir: path.resolve(__dirname, "../GAP/wordpress_plugin_dist/gap_available_dogs/dist"),
+  outputDir: path.resolve(__dirname, "/Users/dinushaw/Local Sites/gaptest/app/public/wp-content/plugins/gap_available_dogs/dist/"),
+  assetsDir: "assets/",
+  filenameHashing: false,
+  
+}
